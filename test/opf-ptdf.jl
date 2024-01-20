4e82770229e58c0641cdee6c8dbfc538882e5a1b
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-20 11:47:00 +0100

    Update opf-ptdf.jl
    change PowerModels to MyPowerModels

--- test/opf-ptdf.jl
+++ test/opf-ptdf.jl
@@ -1,11 +1,11 @@
 
-TESTLOG = Memento.getlogger(PowerModels)
+TESTLOG = Memento.getlogger(MyPowerModels)
 
 @testset "test ptdf-based dc opf" begin
     @testset "5-bus case, LP solver" begin
-        data = PowerModels.parse_file("../test/data/matpower/case5.m")
-        result_va = PowerModels.solve_opf(data, DCPPowerModel, milp_solver)
-        result_pg = PowerModels.solve_opf_ptdf(data, DCPPowerModel, milp_solver)
+        data = MyPowerModels.parse_file("../test/data/matpower/case5.m")
+        result_va = MyPowerModels.solve_opf(data, DCPPowerModel, milp_solver)
+        result_pg = MyPowerModels.solve_opf_ptdf(data, DCPPowerModel, milp_solver)
 
         @test result_va["termination_status"] == OPTIMAL
         @test result_pg["termination_status"] == OPTIMAL
@@ -15,9 +15,9 @@ TESTLOG = Memento.getlogger(PowerModels)
         end
     end
     @testset "5-bus gap case" begin
-        data = PowerModels.parse_file("../test/data/matpower/case5_gap.m")
-        result_va = PowerModels.solve_opf(data, DCPPowerModel, nlp_solver)
-        result_pg = PowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver)
+        data = MyPowerModels.parse_file("../test/data/matpower/case5_gap.m")
+        result_va = MyPowerModels.solve_opf(data, DCPPowerModel, nlp_solver)
+        result_pg = MyPowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver)
 
         @test result_va["termination_status"] == LOCALLY_SOLVED
         @test result_pg["termination_status"] == LOCALLY_SOLVED
@@ -27,10 +27,10 @@ TESTLOG = Memento.getlogger(PowerModels)
         end
     end
     @testset "5-bus with pwl costs" begin
-        data = PowerModels.parse_file("../test/data/matpower/case5_pwlc.m")
+        data = MyPowerModels.parse_file("../test/data/matpower/case5_pwlc.m")
         data["dcline"] = Dict()
-        result_va = PowerModels.solve_opf(data, DCPPowerModel, nlp_solver)
-        result_pg = PowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver)
+        result_va = MyPowerModels.solve_opf(data, DCPPowerModel, nlp_solver)
+        result_pg = MyPowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver)
 
         @test result_va["termination_status"] == LOCALLY_SOLVED
         @test result_pg["termination_status"] == LOCALLY_SOLVED
@@ -40,9 +40,9 @@ TESTLOG = Memento.getlogger(PowerModels)
         end
     end
     @testset "14-bus case" begin
-        data = PowerModels.parse_file("../test/data/matpower/case14.m")
-        result_va = PowerModels.solve_opf(data, DCPPowerModel, nlp_solver)
-        result_pg = PowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver)
+        data = MyPowerModels.parse_file("../test/data/matpower/case14.m")
+        result_va = MyPowerModels.solve_opf(data, DCPPowerModel, nlp_solver)
+        result_pg = MyPowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver)
 
         @test result_va["termination_status"] == LOCALLY_SOLVED
         @test result_pg["termination_status"] == LOCALLY_SOLVED
@@ -53,21 +53,21 @@ TESTLOG = Memento.getlogger(PowerModels)
     end
 
     @testset "no support for zero reference buses" begin
-        data = PowerModels.parse_file("../test/data/matpower/case5.m")
+        data = MyPowerModels.parse_file("../test/data/matpower/case5.m")
         data["bus"]["4"]["bus_type"] = 2
-        @test_throws(TESTLOG, ErrorException, PowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver))
+        @test_throws(TESTLOG, ErrorException, MyPowerModels.solve_opf_ptdf(data, DCPPowerModel, nlp_solver))
     end
 
     @testset "no support for multiple connected components" begin
-        @test_throws(TESTLOG, ErrorException, PowerModels.solve_opf_ptdf("../test/data/matpower/case6.m", DCPPowerModel, nlp_solver))
+        @test_throws(TESTLOG, ErrorException, MyPowerModels.solve_opf_ptdf("../test/data/matpower/case6.m", DCPPowerModel, nlp_solver))
     end
 
     @testset "no support for dclines" begin
-        @test_throws(TESTLOG, ErrorException, PowerModels.solve_opf_ptdf("../test/data/matpower/case3.m", DCPPowerModel, nlp_solver))
+        @test_throws(TESTLOG, ErrorException, MyPowerModels.solve_opf_ptdf("../test/data/matpower/case3.m", DCPPowerModel, nlp_solver))
     end
 
     @testset "no support for switches" begin
-        @test_throws(TESTLOG, ErrorException, PowerModels.solve_opf_ptdf("../test/data/matpower/case5_sw.m", DCPPowerModel, nlp_solver))
+        @test_throws(TESTLOG, ErrorException, MyPowerModels.solve_opf_ptdf("../test/data/matpower/case5_sw.m", DCPPowerModel, nlp_solver))
     end
 
 end
