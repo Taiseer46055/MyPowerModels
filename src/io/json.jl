51eace4144178c81f8815ce8da920341a12315b9
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-20 12:34:25 +0100

    Update json.jl
    change PowerModels to MyPowerModels

--- src/io/json.jl
+++ src/io/json.jl
@@ -1,4 +1,4 @@
-# Parse PowerModels data from JSON exports of PowerModels data structures.
+# Parse MyPowerModels data from JSON exports of MyPowerModels data structures.
 
 function _jsonver2juliaver!(pm_data)
     if haskey(pm_data, "source_version") && isa(pm_data["source_version"], Dict)
@@ -13,11 +13,11 @@ function parse_json(io::Union{IO,String}; kwargs...)::Dict{String,Any}
     _jsonver2juliaver!(pm_data)
 
     if haskey(pm_data, "conductors")
-        Memento.warn(_LOGGER, "The JSON data contains the conductor parameter, but only single conductors are supported.  Consider using PowerModelsDistribution.")
+        Memento.warn(_LOGGER, "The JSON data contains the conductor parameter, but only single conductors are supported.  Consider using MyPowerModelsDistribution.")
     end
 
     if get(kwargs, :validate, true)
-        PowerModels.correct_network_data!(pm_data)
+        MyPowerModels.correct_network_data!(pm_data)
     end
 
     return pm_data
