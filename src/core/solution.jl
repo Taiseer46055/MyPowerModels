f5c51b45c7c444ad5dcd6fe0ede0ed4b3f38b32f
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-20 12:32:38 +0100

    Update solution.jl

--- src/core/solution.jl
+++ src/core/solution.jl
@@ -20,19 +20,19 @@ function sol_data_model!(pm::AbstractPowerModel, solution::Dict)
 end
 
 
-"PowerModels wrapper for the InfrastructureModels `sol_component_value` function."
+"MyPowerModels wrapper for the InfrastructureModels `sol_component_value` function."
 function sol_component_fixed(aim::AbstractPowerModel, n::Int, comp_name::Symbol, field_name::Symbol, comp_ids, constant)
     return _IM.sol_component_fixed(aim, pm_it_sym, n, comp_name, field_name, comp_ids, constant)
 end
 
 
-"PowerModels wrapper for the InfrastructureModels `sol_component_value` function."
+"MyPowerModels wrapper for the InfrastructureModels `sol_component_value` function."
 function sol_component_value(aim::AbstractPowerModel, n::Int, comp_name::Symbol, field_name::Symbol, comp_ids, variables)
     return _IM.sol_component_value(aim, pm_it_sym, n, comp_name, field_name, comp_ids, variables)
 end
 
 
-"PowerModels wrapper for the InfrastructureModels `sol_component_value_edge` function."
+"MyPowerModels wrapper for the InfrastructureModels `sol_component_value_edge` function."
 function sol_component_value_edge(aim::AbstractPowerModel, n::Int, comp_name::Symbol, field_name_fr::Symbol, field_name_to::Symbol, comp_ids_fr, comp_ids_to, variables)
     return _IM.sol_component_value_edge(aim, pm_it_sym, n, comp_name, field_name_fr, field_name_to, comp_ids_fr, comp_ids_to, variables)
 end
