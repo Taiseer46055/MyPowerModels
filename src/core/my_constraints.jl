98aa731dfd6550a135c0887357b742698ee36aa4
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-21 06:37:43 +0100

    Update my_constraints.jl
    Update with bus_id and pm[:bus] instead of pm.gen

--- src/core/my_constraints.jl
+++ src/core/my_constraints.jl
@@ -1,31 +1,37 @@
 # The constraints for inertia and reactive power are defined here.
 
-function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id, gen_tech, delta_P, max_rocof)
+function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_tech::Int, delta_P::Float64, max_rocof::Float64)
     println("Adding minimum system inertia constraint")
+    gen_data = pm[:gen]
+    bus_data = pm[:bus]
+    
+    # Chek if the busnumber exist
+    if !haskey(bus_data, bus_id)
+        error("Bus number $bus_id does not exist in the network")
+    end
+    
     # Check if the required columns exist in the DataFrame
     required_columns = [:bus, :GenTech, :Pg, :H]
     for col in required_columns
-        if !(col in names(pm.gen))
-            error("Required column $col not found in pm.gen DataFrame")
+        if !(col in names(gen_data))
+            error("Required column $col not found in Generator DataFrame")
         end
     end
   
     # Find the specified generator at the specified bus
-    gen_at_bus = findfirst(row -> row[:bus] == bus_id && row[:GenTech] == gen_tech, eachrow(pm.gen))
+    gen_at_bus = findfirst(row -> row[:bus] == bus_id && row[:GenTech] == gen_tech, eachrow(gen_data))
 
     if gen_at_bus == nothing
         error("No generator with GenTech $gen_tech found at bus $bus_id")
     end
     
-    gen_tech = gen_at_bus[:GenTech]
-
     # Calculate H_min based on delta_P and max_rocof
     H_min = delta_P / max_rocof
 
     # Calculate H_sys
     H_sys = 0
     total_Pg = 0
-    for gen in eachrow(pm.gen)
+    for gen in eachrow(gen_data)
         δ = gen[:Pg] > 0 ? 1 : 0
         H_sys += gen[:H] * gen[:Pg] * δ
         total_Pg += gen[:Pg] * δ
