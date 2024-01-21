341ed7bce48cc83cf96b38178f3be45c6a6c279b
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-21 13:27:09 +0100

    Update my_constraints.jl
    change gen_data to a dict

--- src/core/my_constraints.jl
+++ src/core/my_constraints.jl
@@ -1,25 +1,30 @@
 # The constraints for inertia and reactive power are defined here.
-
 function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_tech::Int, delta_P::Float64, max_rocof::Float64)
     println("Add minimum system inertia constraint")
     gen_data = ref(pm, :gen)
     bus_data = ref(pm, :bus)
     
-    # Chek if the busnumber exist
+    # Check if the bus number exists
     if !haskey(bus_data, bus_id)
         error("Bus number $bus_id does not exist in the network")
     end
     
-    # Check if the required columns exist in the DataFrame
-    required_columns = [:bus, :GenTech, :Pg, :H]
-    for col in required_columns
-        if !(col in names(gen_data))
-            error("Required column $col not found in Generator DataFrame")
+    # Check if the required keys exist in gen_data
+    required_keys = [:bus, :GenTech, :Pg, :H]
+    for key in required_keys
+        if !haskey(gen_data, key)
+            error("Required key $key not found in Generator Data")
         end
     end
   
     # Find the specified generator at the specified bus
-    gen_at_bus = findfirst(row -> row[:bus] == bus_id && row[:GenTech] == gen_tech, eachrow(gen_data))
+    gen_at_bus = nothing
+    for (id, gen) in gen_data
+        if gen[:bus] == bus_id && gen[:GenTech] == gen_tech
+            gen_at_bus = gen
+            break
+        end
+    end
 
     if gen_at_bus == nothing
         error("No generator with GenTech $gen_tech found at bus $bus_id")
@@ -31,13 +36,14 @@ function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_
     # Calculate H_sys
     H_sys = 0
     total_Pg = 0
-    for gen in eachrow(gen_data)
+    for (_, gen) in gen_data
         δ = gen[:Pg] > 0 ? 1 : 0
         H_sys += gen[:H] * gen[:Pg] * δ
         total_Pg += gen[:Pg] * δ
     end
     H_sys /= total_Pg
 
-    # Add the constraint
+    # Add the constraint to JuMP
     JuMP.@constraint(pm.model, H_sys >= H_min)
 end
+
