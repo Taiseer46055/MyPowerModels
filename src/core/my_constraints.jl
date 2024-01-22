797465db1cf282a27ed3715a58e6cdbebfec95ff
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-22 19:32:27 +0100

    Update my_constraints.jl

--- src/core/my_constraints.jl
+++ src/core/my_constraints.jl
@@ -1,5 +1,5 @@
 function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_tech::Int, delta_P::Float64, max_rocof::Float64)
-    println("Adding minimum system inertia constraint")
+    println("Add minimum system inertia constraint")
     
     # Retrieve generator and bus data
     gen_data = ref(pm, :gen)
