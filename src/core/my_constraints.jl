78916799a6af004a0e6abc2cdee97329aa777ab0
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-22 10:44:37 +0100

    Update my_constraints.jl
    change :gen_bus to :bus

--- src/core/my_constraints.jl
+++ src/core/my_constraints.jl
@@ -1,31 +1,28 @@
 function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_tech::Int, delta_P::Float64, max_rocof::Float64)
-    println("Add minimum system inertia constraint")
-
+    println("Adding minimum system inertia constraint")
+    
     # Retrieve generator and bus data
     gen_data = ref(pm, :gen)
+    bus_data = ref(pm, :bus)
 
     # Check if the bus number exists
-    if !haskey(ref(pm, :bus), bus_id)
+    if !haskey(bus_data, bus_id)
         error("Bus number $bus_id does not exist in the network")
     end
 
-    # Check if each generator data has the required keys and correct data types
-    required_keys = [:gen_bus, :GenTech, :Pg, :H]
+    # Check if each generator data has the required keys
+    required_keys = [:bus, :GenTech, :Pg, :H]
     for gen in values(gen_data)
         for key in required_keys
             if !haskey(gen, key)
-                error("Required key $key not found in generator data")
-            end
-            if key == :Pg || key == :H
-                if !isa(gen[key], Number) || gen[key] < 0
-                    error("Invalid value for $key in generator data")
-                end
+                gen_id = haskey(gen, :id) ? gen[:id] : "unknown"
+                error("Required key $key not found in Generator with ID $gen_id")
             end
         end
     end
 
     # Find the specified generator at the given bus
-    gen_at_bus = findfirst(gen -> gen[:gen_bus] == bus_id && gen[:GenTech] == gen_tech, values(gen_data))
+    gen_at_bus = findfirst(gen -> gen[:bus] == bus_id && gen[:GenTech] == gen_tech, values(gen_data))
     if gen_at_bus == nothing
         error("No generator with GenTech $gen_tech found at bus $bus_id")
     end
