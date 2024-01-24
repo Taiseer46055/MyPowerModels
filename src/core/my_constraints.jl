e99a2ff9bc989b253747980fbfa411ad8c3a8fa0
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-24 12:33:59 +0100

    Update my_constraints.jl
    update to use gen_id instead of bus_id and GenTech

--- src/core/my_constraints.jl
+++ src/core/my_constraints.jl
@@ -1,3 +1,65 @@
+
+function constraint_min_system_inertia(pm::AbstractPowerModel, gen_id::Int, delta_P::Float64, max_rocof::Float64)
+    println("Adding minimum system inertia constraint")
+    
+    # Retrieve generator and bus data
+    gen_data = ref(pm, :gen)
+    bus_data = ref(pm, :bus)
+
+    # Check if the generator id exists
+    if !haskey(gen_data, gen_id)
+        error("Generator id $gen_id does not exist in the network")
+    end
+
+    # Get the specific generator
+    gen_at_bus = gen_data[gen_id]
+    if haskey(gen_at_bus, "pg")
+        gen_at_bus["pg"] -= delta_P
+    else
+        error("Key 'pg' not found in generator with ID $gen_id")
+    end
+
+    # Set the base frequency f0
+    f0 = 50.0
+    
+    # Calculate P_LOAD as the sum of Pd for all buses with a default value of 0
+    P_LOAD = 0.0
+    for (_, bus) in bus_data
+        if haskey(bus, "pd")
+            P_LOAD += bus["pd"]
+        end
+    end
+
+    # Calculate the minimum system inertia H_min
+    H_min = (delta_P * f0) / (P_LOAD * 2 * max_rocof)
+    
+    H_sys = 0.0
+    total_Pg = 0.0
+    for (_, gen) in gen_data
+        if haskey(gen, "pg") && gen["pg"] > 0
+            H_sys += gen["H"] * gen["pg"]
+            total_Pg += gen["pg"]
+        end
+    end
+    H_sys = total_Pg > 0 ? H_sys / total_Pg : 0.0
+
+    # Add the inertia constraint to the model
+    JuMP.@constraint(pm.model, H_sys >= H_min)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+#=
+
 function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_tech::Int, delta_P::Float64, max_rocof::Float64)
     println("Adding minimum system inertia constraint")
     
@@ -72,4 +134,4 @@ function constraint_min_system_inertia(pm::AbstractPowerModel, bus_id::Int, gen_
     # Add the inertia constraint to the model
     JuMP.@constraint(pm.model, H_sys >= H_min)
 end
-
+=#
