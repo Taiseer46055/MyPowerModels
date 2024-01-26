73e44947b9e7b9ecbb3cdfec3b242f4917c38125
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-26 19:00:49 +0100

    Update variable.jl

--- src/core/variable.jl
+++ src/core/variable.jl
@@ -36,7 +36,9 @@ function variable_system_inertia(pm::AbstractPowerModel; report::Bool=true)
         base_name="H_sys",  
         start = H_sys # Use the calculated value as the start value
     )
+    
     println("Mein H_sys value ist:", H_sys)
+    
     # Add H_sys to the solution components, if necessary
     # report && sol_component_value(pm, :gen, :H_sys, ids(pm, :gen), H_sys)
 
