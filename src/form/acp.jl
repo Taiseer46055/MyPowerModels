0c0c3fccac149a670e8ccaa2ba0f1bbe2ee735d1
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-27 14:58:35 +0100

    Update acp.jl

--- src/form/acp.jl
+++ src/form/acp.jl
@@ -37,10 +37,14 @@ function constraint_min_system_inertia(pm::AbstractACPModel, gen_id::Int, delta_
     if P_load <= 0
         error("Total load P_load is non-positive, which is invalid")
     end
+    println("delta_P",delta_P)
+    println("P_load: ",P_load)
+    println("rocof: ", max_rocof)
     
     # Calculate the minimum system inertia H_min
     H_min = (delta_P * f0) / (P_load * 2 * max_rocof)
-    println(H_min)  
+    println(H_min)
+    
     
     # Add the inertia constraint to the model
     #JuMP.set_optimizer(pm.model, Ipopt.Ipopt.Optimizer)    
