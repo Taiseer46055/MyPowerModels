c22122d5d137cdc567d097592b16be321c01b335
Taiseer46055 <150013742+Taiseer46055@users.noreply.github.com>
2024-01-29 14:06:18 +0100

    Update acp.jl

--- src/form/acp.jl
+++ src/form/acp.jl
@@ -2,7 +2,7 @@
 
 ################################### Start Taiseer Code #########################
 
-function constraint_min_system_inertia(pm::AbstractACPModel, pg_var::Float64, gen_id::Int, delta_P::Float64, max_rocof::Float64)
+function constraint_min_system_inertia(pm::AbstractACPModel, pg_var::Vector{VariableRef}, gen_id::Int, delta_P::Float64, max_rocof::Float64)
     
     println("Adding minimum system inertia constraint to ACPModel")
     
